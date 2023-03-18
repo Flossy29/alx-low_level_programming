eb7972ff68723031ade802136289ada40df5e1b2
flossy <flossykissiwaah@gmail.com>
2023-03-17 17:09:33 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,21 +12,18 @@
 void _is_zero(char *argv[])
 {
 int i, isn1 = 1, isn2 = 1;
-
 for (i = 0; argv[1][i]; i++)
 if (argv[1][i] != '0')
 {
 isn1 = 0;
 break;
 }
-
 for (i = 0; argv[2][i]; i++)
 if (argv[2][i] != '0')
 {
 isn2 = 0;
 break;
 }
-
 if (isn1 == 1 || isn2 == 1)
 {
 printf("0\n");
@@ -44,7 +41,6 @@ exit(0);
 char *_initialize_array(char *ar, int lar)
 {
 int i = 0;
-
 for (i = 0; i < lar; i++)
 ar[i] = '0';
 ar[lar] = '\0';
@@ -69,7 +65,6 @@ if (!isdigit(argv[n][ln]))
 printf("Error\n");
 exit(98);
 }
-
 return (ln);
 }
 
@@ -85,7 +80,6 @@ int main(int argc, char *argv[])
 {
 int ln1, ln2, lnout, add, addl, i, j, k, ca;
 char *nout;
-
 if (argc != 3)
 printf("Error\n"), exit(98);
 ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
